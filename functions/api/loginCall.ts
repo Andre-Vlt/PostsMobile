a109cccef6e6b2e91048509d41ef822f4a4de1cb
André Lucas <andrelvalentim5@gmail.com>
2025-02-06 20:40:29 -0300

    Initial commit

---
+++ functions/api/loginCall.ts
@@ -0,0 +1,26 @@
+export async function LoginCall(username: string, password: string)
+{
+    try
+    {
+        const response = await axios.post('INSERIR ROTA DE LOGIN',{
+            username: username,
+            password: password
+        });
+
+        if(response.status === 200)
+        {
+            const data = response.data;
+            await AsyncStorage.setItem('isLoggedIn', 'true');
+            await AsyncStorage.setItem('userId', data.id_user);
+            await AsyncStorage.setItem('username', data.username);
+        }
+        else
+        {
+            console.log('Erro ao fazer login');
+        }
+    }
+    catch(error)
+    {
+        console.error('Erro ao fazer login: ',error);
+    }
+}
